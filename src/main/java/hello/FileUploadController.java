cd0c5dc268d8ed7472689508308b2d84f7909d6d
travisbrkr1234 <carlos.ochoa@infusionsoft.com>
2015-09-16 12:16:15 -0700

    uploader v1.1 working_state

--- src/main/java/hello/FileUploadController.java
+++ src/main/java/hello/FileUploadController.java
@@ -1,10 +1,13 @@
 package hello;
 
+import java.io.*;
+
 import java.io.BufferedOutputStream;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-
+import java.io.InputStream;
 import java.lang.Character;
 import java.lang.System;
 import java.util.Date;
@@ -12,6 +15,7 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import com.sun.corba.se.spi.orbutil.fsm.Input;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -32,18 +36,29 @@ public class FileUploadController {
                                                  @RequestParam("file") MultipartFile file){
         if (!file.isEmpty()) {
             //Pattern :: check for 13-16 consecutive ints \b\d{13,16}\b OR \b[\d -]{13,}\b checks for split up digits 13+
-            Pattern regexp = Pattern.compile("(([0-9]{4})[-|\\s| ]([0-9]{4})[-|\\s| ]([0-9]{4})[-|\\s| ]([0-9]{4}))|(([0-9]{4})[-|\\s| ]([0-9]{6})[-|\\s| ]([0-9]{5}))");
             String ts =
                     new java.text.SimpleDateFormat("yyyy-MM-dd-hh-mm-ssa").format(new Date()); //Date stamp
             String thisfilename = file.getOriginalFilename(); //Get file name
             //String line = null;
             try {
                 //read file and match against regex
-                //inputStream = new FileInputStream(file);
                 //char ch[] = Character.valueOf(inputStream.read());
                 //Matcher m = regexp.matcher(file); //Matcher
 
                 byte[] bytes = file.getBytes();
+                InputStream inputStream = new ByteArrayInputStream(bytes);
+                Scanner sc = new Scanner(inputStream, "UTF-8");
+            Pattern regexp = Pattern.compile("(([0-9]{4})[-|\\s| ]([0-9]{4})[-|\\s| ]([0-9]{4})[-|\\s| ]([0-9]{4}))|(([0-9]{4})[-|\\s| ]([0-9]{6})[-|\\s| ]([0-9]{5}))");
+                while (sc.hasNextLine()) {
+                    String line = sc.nextLine();
+                    System.out.println(line);
+                     Matcher m = regexp.matcher(line);
+                    if (m.find()) {
+                        System.out.println("found match");
+                    }else
+                        System.out.println("no matches found");
+                }
+                //Matcher m = regexp.matcher(inputStream); //Matcher
                 BufferedOutputStream stream =
                         new BufferedOutputStream(new FileOutputStream(new File(appname + "_" + ts  + "_" + thisfilename)));
                 stream.write(bytes);
