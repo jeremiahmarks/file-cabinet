20fe2d4806460de9753ff64ecf5695b75fa2aa15
travisbrkr1234 <carlos.ochoa@infusionsoft.com>
2015-09-16 14:04:44 -0700

    v1.2 working_state with regex

--- src/main/java/hello/FileUploadController.java
+++ src/main/java/hello/FileUploadController.java
@@ -31,45 +31,47 @@ public class FileUploadController {
         return "You can upload a file by posting to this same URL.";
     }
 
+    //@RequestMapping(value="/fail", method=RequestMethod.GET)
+    //public String uploadfailed() {
+    //     return "redirect:failpage.html";
+    // }
+
     @RequestMapping(value="/upload", method=RequestMethod.POST)
     public @ResponseBody String handleFileUpload(@RequestParam("appname") String appname,
                                                  @RequestParam("file") MultipartFile file){
         if (!file.isEmpty()) {
-            //Pattern :: check for 13-16 consecutive ints \b\d{13,16}\b OR \b[\d -]{13,}\b checks for split up digits 13+
             String ts =
                     new java.text.SimpleDateFormat("yyyy-MM-dd-hh-mm-ssa").format(new Date()); //Date stamp
             String thisfilename = file.getOriginalFilename(); //Get file name
-            //String line = null;
             try {
-                //read file and match against regex
-                //char ch[] = Character.valueOf(inputStream.read());
-                //Matcher m = regexp.matcher(file); //Matcher
-
                 byte[] bytes = file.getBytes();
                 InputStream inputStream = new ByteArrayInputStream(bytes);
                 Scanner sc = new Scanner(inputStream, "UTF-8");
-            Pattern regexp = Pattern.compile("(([0-9]{4})[-|\\s| ]([0-9]{4})[-|\\s| ]([0-9]{4})[-|\\s| ]([0-9]{4}))|(([0-9]{4})[-|\\s| ]([0-9]{6})[-|\\s| ]([0-9]{5}))");
+                Pattern regexp = Pattern.compile("(([0-9]{4})[-|\\s| ]([0-9]{4})[-|\\s| ]([0-9]{4})[-|\\s| ]([0-9]{4}))|(([0-9]{4})[-|\\s| ]([0-9]{6})[-|\\s| ]([0-9]{5}))");
+                boolean hasCreditCardInfo = false;
                 while (sc.hasNextLine()) {
                     String line = sc.nextLine();
-                    System.out.println(line);
-                     Matcher m = regexp.matcher(line);
+                    //System.out.println(line); //verify line data visually
+                    Matcher m = regexp.matcher(line);
                     if (m.find()) {
-                        System.out.println("found match");
-                    }else
-                        System.out.println("no matches found");
+                        hasCreditCardInfo = true;
+                        return "Unable to upload file with credit card data";
+                    }
                 }
-                //Matcher m = regexp.matcher(inputStream); //Matcher
-                BufferedOutputStream stream =
-                        new BufferedOutputStream(new FileOutputStream(new File(appname + "_" + ts  + "_" + thisfilename)));
-                stream.write(bytes);
-                stream.close();
-                return "Thank you for your upload";
+                if (hasCreditCardInfo == false) {
+                    BufferedOutputStream stream =
+                            new BufferedOutputStream(new FileOutputStream(new File(appname + "_" + ts + "_" + thisfilename)));
+                    stream.write(bytes);
+                    stream.close();
+                    return "Thank you for your upload";
+                }
             } catch (Exception e) {
-                return "You failed to upload " + appname + " => " + e.getMessage();
+                return "You failed to upload the file";
             }
         } else {
             return "failed upload because the file was empty.";
         }
+        return "success";
     }
 
 }
